ff7f88da69f774fbc385eaf3eb5d65ba897d7ce8
gayathrie <gayathrie09@gmail.com>
2020-11-08 23:26:28 +1100

    needs to be moved

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,65 @@
 import React from 'react';
 import './App.css';
 
+const BASE_URL = 'http://localhost:5005';
+
 function App() {
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const [loggedIn, setLoggedIn] = React.useState(false);
+
+  const submit = async (e) => {
+    e.preventDefault();
+    const response = await fetch(`${BASE_URL}/admin/auth/login`, {
+      body: {
+        email,
+        password,
+      },
+      headers: { 'Content-Type': 'application/json' },
+      method: 'POST',
+    });
+    if (response.status === 200) {
+      setLoggedIn(true);
+    }
+    console.log(response);
+    return false;
+  };
+
+  if (loggedIn) {
+    return (
+      <div className="App">
+        Youre logged in!
+      </div>
+    );
+  }
+
   return (
-    <></>
+    <div className="App">
+      <form onSubmit={submit}>
+        <label htmlFor="email">
+          Email
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            type="text"
+            id="email"
+            value={email}
+          />
+        </label>
+        <br />
+        <label htmlFor="password">
+          Password
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            id="password"
+            value={password}
+          />
+        </label>
+        <br />
+        <input type="submit" value="login" />
+        <br />
+      </form>
+    </div>
   );
 }
 
