5dc3bdfe8cdbb7b9b45bc8f71c52abc707b7e68a
Arth Patel <arthpatelf50@gmail.com>
2020-11-09 13:12:08 +0530

    Added navBBar with working logout button

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { postMethodOptions } from '../options';
+
+const BASE_URL = 'http://localhost:5005';
+
+export default () => {
+  const history = useHistory();
+  const handleLogOut = async () => {
+    postMethodOptions.headers.Authorization = localStorage.getItem('token');
+    const response = await fetch(`${BASE_URL}/admin/auth/logout`, postMethodOptions);
+    if (response.status === 200) {
+      history.replace('/login');
+    } else {
+      alert('error occured please try again');
+    }
+  };
+  return (
+    <>
+      <div className="navBar">
+        <button type="button" onClick={handleLogOut}>LogOut</button>
+      </div>
+    </>
+  );
+};
