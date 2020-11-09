639d59d822099ed28e1d33c9c997202482debdea
Arth Patel <arthpatelf50@gmail.com>
2020-11-09 13:12:21 +0530

    updated dashoard with navbar

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import NavBar from '../components/NavBar';
 // const BASE_URL = 'http://localhost:5005';
 export default () => (
   <div>
+    <NavBar />
     Youre logged in!
   </div>
 );
