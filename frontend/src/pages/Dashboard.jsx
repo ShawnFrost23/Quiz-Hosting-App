a8dd0eb007f19a149384f9b2dea01d18141eedd7
Arth Patel <arthpatelf50@gmail.com>
2020-11-10 13:58:08 +0530

    fetching quizzes for user done

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,13 +1,31 @@
+/* eslint-disable */
 import React from 'react';
 import NavBar from '../components/NavBar';
 import CreateNewGameBar from '../components/CreateNewGameBar';
-// const BASE_URL = 'http://localhost:5005';
+import { getMethodOptions } from '../options';
+
+const BASE_URL = 'http://localhost:5005';
+
 function Dashboard() {
+  const [games, setGames] = React.useState([]);
+  const getListofGames = async () => {
+    getMethodOptions.headers.Authorization = localStorage.getItem('token');
+    const response = await fetch(`${BASE_URL}/admin/quiz`, getMethodOptions);
+    const response2 = await response.json();
+    console.log(response2.quizzes);
+    setGames(response2.quizzes);
+    console.log(games);
+  };
+  React.useEffect(() => {
+    getListofGames();
+  }, [])
   return (
     <>
       <NavBar />
       <CreateNewGameBar />
-      Youre logged in!
+      <div>
+        Create Games Appear here
+      </div>
     </>
   );
 }
