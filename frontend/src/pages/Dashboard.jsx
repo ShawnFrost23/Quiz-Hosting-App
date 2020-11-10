7f732a29026210a94f1e93a5697355cfd6efeffc
Arth Patel <arthpatelf50@gmail.com>
2020-11-10 10:40:20 +0530

    changed to have fewer nested div's

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import NavBar from '../components/NavBar';
 // const BASE_URL = 'http://localhost:5005';
 export default () => (
-  <div>
+  <>
     <NavBar />
     Youre logged in!
-  </div>
+  </>
 );
