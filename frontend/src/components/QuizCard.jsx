225c50722509d0c84738c7ad0d4bac447f2a1420
Arth Patel <arthpatelf50@gmail.com>
2020-11-12 12:54:57 +0530

    basic quiz card component showing contents of quiz

--- frontend/src/components/QuizCard.jsx
+++ frontend/src/components/QuizCard.jsx
@@ -3,9 +3,8 @@
 import React from 'react';
 
 function QuizCard({id, quizName, thumbnail}) {
-  const [quizId, setQuizId] = React.useEffect('');
-  setQuizId(id)
-  const [quizState, setQuizState] = React.useEffect(false);
+  const [quizId, setQuizId] = React.useState(id);
+  const [quizState, setQuizState] = React.useState(false);
   if (quizState) {
     return (
       <p>To be implemented</p>
@@ -13,14 +12,26 @@ function QuizCard({id, quizName, thumbnail}) {
     );
   }
   const altText = `Image for quiz Id: ${quizId}`
+
+  const editQuizButtonHandler = () => {
+    console.log('Edit Quiz Button Pressed');
+  }
+
+  const deleteQuizButtonHandler = () => {
+    console.log('Delete Quiz Button Pressed');
+  }
+
+  const startQuizButtonHandler = () => {
+    console.log('Start Quiz Button Pressed');
+  }
   return (
-    <div className="quizCard" styles={{margin: '1px solid red'}}>
+    <div className="quizCard">
       <p>{quizName}</p>
       <img src={thumbnail} alt={altText} />
       {/* <QuestionsDetailComponent /> */}
-      <button className="editQuizButton">Edit</button>
-      <button className="deleteQuizButton">Delete</button>
-      <button className="startQuizButton">Start</button>
+      <button className="editQuizButton" onClick={editQuizButtonHandler}>Edit</button>
+      <button className="deleteQuizButton" onClick={deleteQuizButtonHandler}>Delete</button>
+      <button className="startQuizButton" onClick={startQuizButtonHandler}>Start Quiz</button>
     </div>
   );
 };
