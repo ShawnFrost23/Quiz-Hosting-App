c78475ff3f9c73f2494f89685b32449d7000c54b
Arth Patel <arthpatelf50@gmail.com>
2020-11-12 19:08:59 +0530

    Working create new game button and instant refresh

--- frontend/src/components/CreateNewGameBar.jsx
+++ frontend/src/components/CreateNewGameBar.jsx
@@ -1,12 +1,36 @@
+/* eslint-disable */
 import React from 'react';
+import { postMethodOptions } from '../options';
+import { getListofGames } from '../pages/Dashboard';
 
-export default () => {
-  const handleNewGameButton = () => {
-    console.log('New game Button Clicked');
+
+const BASE_URL = 'http://localhost:5005';
+const token = localStorage.getItem('token');
+function CreateNewGameBar({setGameFunction}) {
+  const [newGameName, setNewGameName] = React.useState('');
+  
+  const handleNewGameButton = async () => {
+    if(newGameName.length > 0) {
+      postMethodOptions.headers.Authorization = token;
+      const newBody = {
+        'name': newGameName,
+      };
+      postMethodOptions.body = JSON.stringify(newBody);
+      const response = await fetch(`${BASE_URL}/admin/quiz/new`, postMethodOptions)
+      getListofGames(setGameFunction)
+    }
+    
   };
   return (
     <div className="createNewGameBar">
+      <input
+        onChange={(e) => setNewGameName(e.target.value)}
+        type="text"
+        placeholder="Enter New Game Name"
+      />
       <button type="button" onClick={handleNewGameButton}>Create New Game</button>
     </div>
   );
 };
+
+export default CreateNewGameBar;
