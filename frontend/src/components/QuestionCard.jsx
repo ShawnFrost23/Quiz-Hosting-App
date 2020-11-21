e36b5bcf44820b0058217bb0fcb56835a2e6b384
Arth Patel <arthpatelf50@gmail.com>
2020-11-21 20:10:08 +0530

    better refactored code

--- frontend/src/components/QuestionCard.jsx
+++ frontend/src/components/QuestionCard.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useHistory, useParams } from 'react-router-dom';
+import { putMethodOptions, getMethodOptions } from '../options';
 
 export default function QuestionCard(props) {
   const history = useHistory();
@@ -12,15 +13,11 @@ export default function QuestionCard(props) {
   id1 = id1.substring(1);
 
   React.useEffect(() => {
+    getMethodOptions.headers.Authorization = ber;
+    getMethodOptions.headers.accept = 'application/json';
+
     async function getQuiz() {
-      const response = await fetch(`${BASE_URL}/admin/quiz/${id1}`, {
-        headers: {
-          accept: 'application/json',
-          Authorization: ber,
-          'Content-Type': 'application/json',
-        },
-        method: 'GET',
-      });
+      const response = await fetch(`${BASE_URL}/admin/quiz/${id1}`, getMethodOptions);
       if (response.status === 200) {
         const response2 = await response.json();
         setGetData(response2.questions);
@@ -33,15 +30,10 @@ export default function QuestionCard(props) {
   }, [ber, id1]);
 
   async function putQuiz(newBody) {
-    const response = await fetch(`${BASE_URL}/admin/quiz/${id1}`, {
-      body: JSON.stringify(newBody),
-      headers: {
-        accept: 'application/json',
-        Authorization: ber,
-        'Content-Type': 'application/json',
-      },
-      method: 'PUT',
-    });
+    putMethodOptions.headers.Authorization = ber;
+    putMethodOptions.headers.accept = 'application/json';
+    putMethodOptions.body = JSON.stringify(newBody);
+    const response = await fetch(`${BASE_URL}/admin/quiz/${id1}`, putMethodOptions);
     if (response.status === 200) {
       const response2 = await response.json();
       console.log(response2);
@@ -79,8 +71,8 @@ export default function QuestionCard(props) {
       <div className="d-flex justify-content-around align-items-center flex-column w-400 h-500 mx-20 my-20 px-5 py-5 border rounded bg-light">
         <h3>{title}</h3>
         <img className="w-full h-half border rounded" src={thumbnail} alt={altText} />
-        <button className="w-half btn btn-primary btn-rounded" id={id} type="button" onClick={onClickRoutePage}>Edit</button>
-        <button className="w-half btn btn-danger btn-rounded" id={id} type="button" onClick={DeleteQuestion}>Delete</button>
+        <button className="w-half btn btn-primary btn-rounded" id={id} type="button" onClick={onClickRoutePage} aria-label="Edit Question">Edit</button>
+        <button className="w-half btn btn-danger btn-rounded" id={id} type="button" onClick={DeleteQuestion} aria-label="Delete Question">Delete</button>
       </div>
     </>
   );
@@ -89,5 +81,9 @@ export default function QuestionCard(props) {
 QuestionCard.propTypes = {
   id: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
-  thumbnail: PropTypes.string.isRequired,
+  thumbnail: PropTypes.string,
+};
+
+QuestionCard.defaultProps = {
+  thumbnail: null,
 };
