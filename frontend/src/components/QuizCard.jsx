789fb774a3b5a5e920b5f24b879f011d10808e9e
Arth Patel <arthpatelf50@gmail.com>
2020-11-21 16:17:56 +0530

    linting errors solved

--- frontend/src/components/QuizCard.jsx
+++ frontend/src/components/QuizCard.jsx
@@ -1,83 +1,82 @@
-/* eslint-disable */
-
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import { deleteMethodOptions, postMethodOptions } from '../options';
+// eslint-disable-next-line
 import { getListofGames } from '../pages/Dashboard';
-import { useHistory, useParams } from 'react-router-dom';
 import QuizDetailComponent from './QuizDetailComponent';
-
 
 const BASE_URL = 'http://localhost:5005';
 
+// eslint-disable-next-line
 function QuizCard({id, quizName, thumbnail, setGameFunction, status}) {
-  const [quizId, setQuizId] = React.useState(id);
+  const [quizId] = React.useState(id);
   const [quizState, setQuizState] = React.useState(false);
   const history = useHistory();
-  const altText = `Image for quiz Id: ${quizId}`
+  const altText = `Image for quiz Id: ${quizId}`;
 
   const editQuizButtonHandler = () => {
     localStorage.setItem('quizID', id);
     history.push(`/editpage/:${id}`);
-  }
+  };
 
   const deleteQuizButtonHandler = async () => {
     deleteMethodOptions.headers.Authorization = localStorage.getItem('token');
-    const response = await fetch(`${BASE_URL}/admin/quiz/${quizId}`, deleteMethodOptions)
+    const response = await fetch(`${BASE_URL}/admin/quiz/${quizId}`, deleteMethodOptions);
     if (response.status === 200) {
-      getListofGames(setGameFunction)
-      window.location.reload(false)
+      getListofGames(setGameFunction);
+      window.location.reload(false);
     }
-  }
+  };
 
   const startQuizButtonHandler = async () => {
     postMethodOptions.headers.Authorization = localStorage.getItem('token');
     const response = await fetch(`${BASE_URL}/admin/quiz/${quizId}/start`, postMethodOptions);
     if (response.status === 200) {
-      setQuizState(true)
+      setQuizState(true);
     }
-  }
+  };
 
   const stopQuizButtonHandler = async () => {
     postMethodOptions.headers.Authorization = localStorage.getItem('token');
     const response = await fetch(`${BASE_URL}/admin/quiz/${quizId}/end`, postMethodOptions);
     if (response.status === 200) {
-      setQuizState(false)
+      setQuizState(false);
     }
-  }
+  };
 
   const advanceQuizButtonHandler = async () => {
-    console.log('Advance Quiz Button Pressed')
     postMethodOptions.headers.Authorization = localStorage.getItem('token');
     const response = await fetch(`${BASE_URL}/admin/quiz/${quizId}/advance`, postMethodOptions);
     if (response.status !== 200) {
-      setQuizState(false)
+      setQuizState(false);
     }
-  }
+  };
+
   if (quizState || status) {
     return (
       <div className="quizCardActive">
         <p>{quizName}</p>
         <img src={thumbnail} alt={altText} />
-        <QuizDetailComponent 
+        <QuizDetailComponent
           quizId={quizId}
         />
-        <button className="advanceQuizButton" onClick={advanceQuizButtonHandler}>Next Question</button>
-        <button className="stopQuizButton" onClick={stopQuizButtonHandler}>Stop Quiz</button>
-    </div>
+        <button className="advanceQuizButton" type="button" onClick={advanceQuizButtonHandler}>Next Question</button>
+        <button className="stopQuizButton" type="button" onClick={stopQuizButtonHandler}>Stop Quiz</button>
+      </div>
     );
   }
   return (
     <div className="quizCard">
       <p>{quizName}</p>
       <img src={thumbnail} alt={altText} />
-      <QuizDetailComponent 
+      <QuizDetailComponent
         quizId={quizId}
       />
-      <button className="editQuizButton" onClick={editQuizButtonHandler}>Edit</button>
-      <button className="deleteQuizButton" onClick={deleteQuizButtonHandler}>Delete</button>
-      <button className="startQuizButton" onClick={startQuizButtonHandler}>Start Quiz</button>
+      <button className="editQuizButton" type="button" onClick={editQuizButtonHandler}>Edit</button>
+      <button className="deleteQuizButton" type="button" onClick={deleteQuizButtonHandler}>Delete</button>
+      <button className="startQuizButton" type="button" onClick={startQuizButtonHandler}>Start Quiz</button>
     </div>
   );
-};
+}
 
 export default QuizCard;
