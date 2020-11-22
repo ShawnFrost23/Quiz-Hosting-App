36cc05dc080ca280ea1421c7dedc34c89eb82a4a
Arth Patel <arthpatelf50@gmail.com>
2020-11-22 13:28:26 +0530

    final checked with accesibilty complete

--- frontend/src/pages/EditQuestion.jsx
+++ frontend/src/pages/EditQuestion.jsx
@@ -1,6 +1,5 @@
+// File Checked
 import React from 'react';
-// import { useHistory } from 'react-router-dom';
-// import { postMethodOptions } from '../options';
 import EditForm from '../components/QuestionEditForm';
 import NavBar from '../components/NavBar';
 
